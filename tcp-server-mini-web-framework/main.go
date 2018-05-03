2c04df8c52dffb5a5e31fd55cf845f6a3f2f8aed
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-05-03 14:03:22 +0200

    Setup initial templates loading and scaffold for main function

---
+++ tcp-server-mini-web-framework/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"strings"
+	"net"
+	"log"
+)
+
+var tpl *template.Template
+
+func init()  {
+	tpl = template.Must(template.New("").Funcs(tplHelpers).ParseGlob(`templates/*`))
+}
+
+var tplHelpers = template.FuncMap{
+	"capitalize": func (s string) string { return strings.Title(s) },
+}
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil { log.Fatalln(err) }
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil { log.Fatalln(err) }
+		go handle(conn)
+	}
+}
