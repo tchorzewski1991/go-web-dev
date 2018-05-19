66e704ef6500ccb7ab1006be8d41f1b80c70abb8
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-05-19 09:24:04 +0200

    Create link package and design public api interface

---
+++ exercises/http-link-parser/link.go
@@ -0,0 +1,9 @@
+package link
+
+import "io"
+
+// Parse will take an HTML document and will return a slice of links parsed
+// from it.
+func Parse(r io.Reader) ([]Link, error) {
+	return nil, nil
+}
