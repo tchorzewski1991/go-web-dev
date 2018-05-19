fc09d8d1030c95ee3b781ec67f708d7e3f11a43e
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-05-19 11:25:18 +0200

    Implement logic scaffold for Parse() function

--- exercises/http-link-parser/link.go
+++ exercises/http-link-parser/link.go
@@ -1,6 +1,9 @@
 package link
 
-import "io"
+import (
+	"golang.org/x/net/html"
+	"io"
+)
 
 // Link is a representation of link (<a href="...">) in HTML document.
 type Link struct {
@@ -11,5 +14,20 @@ type Link struct {
 // Parse will take an HTML document and will return a slice of links parsed
 // from it.
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+
+	doc, err := html.Parse(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	nodes := findLinks(doc)
+
+	var links []Link
+
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+
+	return links, nil
 }
